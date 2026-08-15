031db4acc6226f5f1440b0ec55b1b659645e5b68
wangpan <2013248845@qq.com>
2026-08-15 13:10:43 +0800

    1

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,10 +6,10 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "WP's Blog",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "Welcome to WP's Blog",
 
 	// 站点 URL
 	site_url: "https://wangpan6.top",
